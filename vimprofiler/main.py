9d0eb81ebbff2d0f57ba5dfc756d2a1ae497a197
jason.j.xiong@vanderbilt.edu <jason.j.xiong@gmail.com>
2015-03-23 21:17:38 -0500

    now waits on the external pid

--- vimprofiler/main.py
+++ vimprofiler/main.py
@@ -1,6 +1,19 @@
 import subprocess
+import os
 
 
-args = ['gnome-terminal', '-x', 'sh', '-c',
-        'vim --startuptime vimprofile-startuptime.log -S plugin.vim; bash']
-subprocess.call(args)
+vim_command = 'vim --startuptime vimprofile-startuptime.log -S plugin.vim'
+args = ['gnome-terminal', '-e', vim_command]
+proc = subprocess.Popen(args).pid
+
+os.waitid(os.P_PID, int(proc), os.WEXITED)
+
+proc = int(subprocess.Popen(['pgrep', '-f', vim_command],
+                        stdout=subprocess.PIPE).stdout.read().decode('utf-8').rstrip())
+print(proc)
+while True:
+    try:
+        os.kill(proc, 0)
+    except OSError:
+        break
+

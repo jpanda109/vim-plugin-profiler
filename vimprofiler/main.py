67b10090d697cee0e9cb67034103c82c626b9b03
jason.j.xiong@vanderbilt.edu <jason.j.xiong@gmail.com>
2015-03-26 22:21:29 -0500

    hi

--- vimprofiler/main.py
+++ vimprofiler/main.py
@@ -9,20 +9,30 @@ def print_hello_world():
     print('hello world')
 
 
-if __name__=='__main__':
-    startup_file = 'vimprofile-startuptime.log'
+if __name__ == '__main__':
+    working_path = os.path.dirname(os.path.abspath(__file__))
+    # file initializations
+    startup_file = os.path.join(working_path, 'vimprofile-startuptime.log')
+    plugin_file = os.path.join(working_path, 'plugin.vim')
+    print(working_path)
 
-    os.remove(startup_file)
-    vim_command = 'vim --startuptime vimprofile-startuptime.log -S plugin.vim'
+    try:
+        os.remove(startup_file)
+    except FileNotFoundError:
+        pass
+    vim_command = 'vim --startuptime %r -S %r' % (startup_file, plugin_file)
     args = ['gnome-terminal', '-e', vim_command]
-
 
     proc = subprocess.Popen(args).pid
 
     os.waitid(os.P_PID, int(proc), os.WEXITED)
 
-    proc = int(subprocess.Popen(['pgrep', '-f', vim_command],
+    try:
+        proc = int(subprocess.Popen(['pgrep', '-f', vim_command[:10]],
                                 stdout=subprocess.PIPE).stdout.read().decode('utf-8').rstrip())
+        print(proc)
+    except ValueError:
+        proc = 0
 
     # sample threading before blocking for waiting on vim
     t = threading.Thread(target=print_hello_world, daemon=True)

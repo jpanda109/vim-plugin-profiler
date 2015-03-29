9865ab8a9263ec52066115df29c5c9d3e930ab64
jason.j.xiong@vanderbilt.edu <jason.j.xiong@gmail.com>
2015-03-29 17:38:26 -0500

    wrote curses wrapper

--- sandbox/curse_ex.py
+++ sandbox/curse_ex.py
@@ -1,6 +1,18 @@
 import curses
 import threading
 import queue
+import sys
+from functools import wraps
+
+
+def safe_curses(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        try:
+            f(*args, **kwargs)
+        except:
+            pass
+    return wrapper
 
 
 def process_input(input_queue, screen):
@@ -8,24 +20,30 @@ def process_input(input_queue, screen):
         keypress = screen.getkey()
         input_queue.put(keypress)
 
-if __name__ == "__main__":
-    myscreen = curses.initscr()
 
-    dimensions = myscreen.getmaxyx()
-
-    myscreen.border(0)
-    myscreen.addstr(12, 25, "Python curses in action!")
-    myscreen.refresh()
+@safe_curses
+def main(screen):
+    screen.border(0)
+    screen.addstr(12, 25, "Python curses in action!")
+    screen.refresh()
 
     input_queue = queue.Queue()
     input_thread = threading.Thread(target=process_input,
-                                    args=(input_queue, myscreen), daemon=True)
+                                    args=(input_queue, screen), daemon=True)
     input_thread.start()
     while True:
         if not input_queue.empty():
             keypress = input_queue.get()
             if keypress == 'q' or keypress == 'Q':
                 break
-            myscreen.addstr(30, 30, keypress)
+            screen.addstr(30, 30, keypress)
 
+
+if __name__ == "__main__":
+    myscreen = curses.initscr()
+    curses.noecho()
+
+    main(myscreen)
+
+    curses.echo()
     curses.endwin()

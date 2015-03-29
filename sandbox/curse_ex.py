0f55cbeff2804b1126f19ca79979065469834e44
jason.j.xiong@vanderbilt.edu <jason.j.xiong@gmail.com>
2015-03-29 17:27:41 -0500

    curse example with input

--- sandbox/curse_ex.py
+++ sandbox/curse_ex.py
@@ -1,5 +1,12 @@
 import curses
+import threading
+import queue
 
+
+def process_input(input_queue, screen):
+    while True:
+        keypress = screen.getkey()
+        input_queue.put(keypress)
 
 if __name__ == "__main__":
     myscreen = curses.initscr()
@@ -9,6 +16,16 @@ if __name__ == "__main__":
     myscreen.border(0)
     myscreen.addstr(12, 25, "Python curses in action!")
     myscreen.refresh()
-    myscreen.getch()
+
+    input_queue = queue.Queue()
+    input_thread = threading.Thread(target=process_input,
+                                    args=(input_queue, myscreen), daemon=True)
+    input_thread.start()
+    while True:
+        if not input_queue.empty():
+            keypress = input_queue.get()
+            if keypress == 'q' or keypress == 'Q':
+                break
+            myscreen.addstr(30, 30, keypress)
 
     curses.endwin()

fe80b7d78ccd086960414eef7f5c6ca7bb96d2dc
jason.j.xiong@vanderbilt.edu <jason.j.xiong@gmail.com>
2015-03-31 10:17:14 -0500

    calculating proc in task

--- vimprofiler/plugin/log_info.py
+++ vimprofiler/plugin/log_info.py
@@ -6,22 +6,8 @@ import json
 command = vim.eval('a:command')
 pipename = os.environ['VIMUALIZER_PIPE_NAME']
 with open(pipename, 'w') as pipe:
-    proc_file_name = '/proc/' + str(os.getpid()) + '/stat'
-    with open(proc_file_name, 'r') as proc_file:
-        stats = proc_file.readline().split(' ')
-        utime = stats[13]
-        stime = stats[14]
-        cutime = stats[15]
-        cstime = stats[15]
-
     data = {
         'time': time.time(),
         'command': command,
-        'utime': utime,
-        'stime': stime,
-        'cutime': cutime,
-        'cstime': cstime
     }
     data_string = json.dump(data, pipe)
-    # pipe.write(data_string)
-    # pipe.write(command.encode('utf-8'))
--- vimprofiler/src/tasks.py
+++ vimprofiler/src/tasks.py
@@ -1,4 +1,41 @@
+import os
+import time
+
+
+HERTZ = 250
+
+
 def process_input(input_queue, screen):
     while True:
         keypress = screen.getkey()
         input_queue.put(keypress)
+
+
+def calculate_cpu(cpu_queue, interval):
+    proc_file_name = '/proc/' + str(os.getpid()) + '/stat'
+    utime_prev = 0
+    utime_next = 0
+    stime_prev = 0
+    stime_next = 0
+    cutime_prev = 0
+    cutime_next = 0
+    cstime_prev = 0
+    cstime_next = 0
+    while True:
+        with open(proc_file_name, 'r') as proc_file:
+            stats = proc_file.readline().split(' ')
+        utime_next = stats[13]
+        stime_next = stats[14]
+        cutime_next = stats[15]
+        cstime_next = stats[16]
+        total_time = (utime_next - utime_prev) + (stime_next - stime_prev)
+        total_time += (cutime_next - cutime_prev) + (cstime_next - cstime_prev)
+        cpu_usage = 100 * ((total_time / HERTZ) / interval)
+        cpu_queue.put((cpu_usage, time.time()))
+
+        time.sleep(interval)
+
+        utime_prev = utime_next
+        stime_prev = stime_next
+        cutime_prev = cutime_next
+        cstime_prev = cstime_next

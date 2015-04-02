a1226392e4686be0c4b271ef19bd6ba21848f9c7
jason.j.xiong@vanderbilt.edu <jason.j.xiong@gmail.com>
2015-04-02 10:31:38 -0500

    added ValueEvent

---
+++ vimprofiler/src/lib/utils.py
@@ -0,0 +1,28 @@
+import threading
+
+
+class ValueEvent():
+
+    """ container for multiple threading.Event(s) """
+
+    def __init__(self, value=None):
+        self.event = threading.Event()
+        self.value = value
+
+    def is_set(self, *events):
+        return self.event.is_set()
+
+    def set(self, value):
+        self.value = value
+        self.event.set()
+
+    def clear(self, value=None):
+        if value is not None:
+            self.value = value
+        self.event.clear()
+
+    def wait(self, timeout=None):
+        self.event.wait(timeout=timeout)
+
+    def get_value(self):
+        return self.value

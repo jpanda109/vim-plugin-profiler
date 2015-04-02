54ddefb1e28e1d3848497fb604dc4092f352e988
jason.j.xiong@vanderbilt.edu <jason.j.xiong@gmail.com>
2015-04-02 15:44:43 -0500

    added startup mode

---
+++ vimprofiler/src/modes/startup.py
@@ -0,0 +1,12 @@
+logging.basicConfig(filename='logging_stuff.log', level=logging.DEBUG)
+
+
+def _exit_mode(threads, screen):
+
+    """ gracefully clean up everything this mode was doing """
+
+    pass
+
+
+def main(screen):
+    y, x = screen.getmaxyx()

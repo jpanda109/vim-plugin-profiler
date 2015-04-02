a92fb618819fe875edd14a6fc26a205d5b59b347
jason.j.xiong@vanderbilt.edu <jason.j.xiong@gmail.com>
2015-04-02 17:05:26 -0500

    should now work in xubuntu vm as well

--- vimprofiler/plugin/log_info.py
+++ vimprofiler/plugin/log_info.py
@@ -1,11 +1,9 @@
 import vim
-import os
 import time
 import json
 
 command = vim.eval('a:command')
-pipename = os.environ['VIMUALIZER_PIPE_NAME']
-with open(pipename, 'w') as pipe:
+with open('tmpfifo', 'w') as pipe:
     data = {
         'time': time.time(),
         'command': command,

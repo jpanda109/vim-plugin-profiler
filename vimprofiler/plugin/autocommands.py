069124ba85c22bd64115c1eb41f5acd74252bf5d
matt <matthew.l.lee@vanderbilt.edu>
2015-04-23 21:05:10 -0500

    documentation

--- vimprofiler/plugin/autocommands.py
+++ vimprofiler/plugin/autocommands.py
@@ -1,6 +1,9 @@
 import vim
 import time
 
+#Takes the commands, reads them, and sends them to VPLogInfo
+#Which then sends it to the python script
+
 with open('plugin/commands', 'r') as command_file:
     commands = command_file.readlines()
 
@@ -8,5 +11,5 @@ start_time = time.time()
 
 for command in commands:
     command = command.rstrip()
-    vim.command("autocmd " + command + " * :call VPLogInfo('" 
+    vim.command("autocmd " + command + " * :call VPLogInfo('"
         + command + "','" + str(start_time) + "')")
--- vimprofiler/plugin/log_info.py
+++ vimprofiler/plugin/log_info.py
@@ -2,6 +2,8 @@ import vim
 import time
 import json
 
+#Takes in command & start time from vim and passes it into the pipe
+
 command = vim.eval('a:command')
 start_time = vim.eval('a:start_time')
 
